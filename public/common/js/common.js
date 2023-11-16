121ebcf837d5f7ce30fe19e3b51dea1df30d798c
YOON <stary92@daum.net>
2023-11-16 21:45:07 +0900

    #231116 반응형 일부 최적화, 추가작업

--- public/common/js/common.js
+++ public/common/js/common.js
@@ -12,13 +12,27 @@ $(document).ready(function () {
             $(".popuplayer_list > div").eq(curIdx).addClass("popped");
         }
     });
+
     // 상세페이지 창 스팬
     $(".gray_span_btn").on("click", function (e) {
         e.preventDefault();
         $(".descwrap").toggleClass("spanned");
     });
 
-    // tab_list 동작 및 높이값 조정
+    // 공통 탭
+    $(".tabmenu .tab_list > li").first().addClass("active");
+    $(".tabmenu .tabview_list").children().first().addClass("active");
+    $(".tabmenu .tab_list > li > a").on("click", function (e) {
+        e.preventDefault();
+        var Idx = $(this).parents("li").index();
+
+        $(".tabmenu .tab_list > li").removeClass("active");
+        $(this).parent().addClass("active");
+        $(".tabmenu .tabview_list").children().removeClass("active");
+        $(".tabmenu .tabview_list").children().eq(Idx).addClass("active");
+    });
+
+    // edu_tabpage tab_list 동작
     $(".swiperTab .tab_list > li").first().addClass("active");
     $(".tabview_list > ul").first().addClass("active");
     $(".swiperTab .tab_list > li > a").on("click", function (e) {
@@ -31,9 +45,10 @@ $(document).ready(function () {
         $(".tabview_list > ul").eq(Idx).addClass("active");
     });
 
+    // .sticky_rsz 항목 높이값
     function hdrRsz() {
         var hdrHeight = $("header").innerHeight();
-        $(".swiperTab").css("top", hdrHeight + 9);
+        $(".sticky_rsz").css("top", hdrHeight + 9);
     }
     hdrRsz();
     $(window).resize(function () {
